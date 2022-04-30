14ee8d2abadce2813d81f50947dd9da9f97ba987
Anowar Hossen Farvez <farvez165@gmail.com>
2022-05-01 00:25:18 +0600

    Account Creation controller page and fxml file for Employee Section

---
+++ Banking System/src/application/EmpCreateAccController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class EmpCreateAccController {
+	public void empHomePage(ActionEvent e) {
+		try {
+		Stage primaryStage = new Stage();
+		AnchorPane root = FXMLLoader.load(getClass().getResource("empHome.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+		catch(Exception we) {
+			
+		}
+	}
+
+}
