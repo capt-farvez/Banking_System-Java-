c08704882ef1641064f337ff8697019e60c468e7
Anowar Hossen Farvez <farvez165@gmail.com>
2022-05-10 02:14:19 +0600

    Multiple window merze in a single window

--- Banking System/src/application/empViewController.java
+++ Banking System/src/application/empViewController.java
@@ -1,20 +1,27 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class empViewController {
+	@FXML
+	public TextField accNum;
+	@FXML
+	public TextField nid;
+	
 	public void empHomePage(ActionEvent e) {
 		try {
-		Stage primaryStage = new Stage();
+		
 		AnchorPane root = FXMLLoader.load(getClass().getResource("empHome.fxml"));
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		Main.primaryStage.setScene(scene);
+		Main.primaryStage.show();
 		
 	}
 		catch(Exception we) {
